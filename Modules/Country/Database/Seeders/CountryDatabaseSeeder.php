19dfdcf88a4a15a8ed17132688e5f61b7144c72a
José Guerrero <jose.guerrero.carrizo@gmail.com>
2020-08-23 17:11:10 -0500

    Módulo de países (#5)
    * Módulo paises
    
    Creación módulo de países
    
    * Obtener todos los países
    
    Método para obtener todos los países
    
    * Eliminar carpetas de recursos
    
    Eliminación de carpetas que no se utilizaran en el módulo
    
    * Repositorio países
    
    Creando repositorio de países
    
    * Obtener todos los países
    
    Método para obtener todos los países
    
    * Obtener un país
    
    Método para obtener un país por su identificador
    
    * Almacenar un país
    
    Método para almacenar un país
    
    * Actualizar un país
    
    Método para actualizar un país
    
    * Eliminar país
    
    Método para eliminar un país
    
    * Estándar en nombre de métodos
    
    Cambiando el estándar para los nombres en los métodos del repositorio
    
    * Prueba unitaria: Obtener todos los países
    
    Creación de set de prueba para obtener todos los países
    
    * Prueba unitaria: Obtener un país por su identificador
    
    Creación de set de pruebas para obtener un país por su identificador
    
    * Error solicitando token de acceso
    
    Error validando credenciales de acceso para generar token de acceso
    
    * Sintaxis documentación
    
    Mejorando la sintaxis de la documentación en el módulo
    
    * Archivos de configuraciones phpstorm
    
    Configuraciones para el editor phpstorm
    
    * Pruebas unitarias: Almacenar país y validar campo nombre
    
    Método para almacenar un país y validar el nombre que se envía
    
    * Pruebas unitaria: Actualizar país
    
    Creación de set de pruebas para actualizar un país
    
    * Pruebas unitarias: Eliminar país
    
    Creación set de pruebas para eliminar un país
    
    * Módulo paises
    
    Creación módulo de países
    Obtener todos los países
    
    Método para obtener todos los países
    Eliminar carpetas de recursos
    
    Eliminación de carpetas que no se utilizaran en el módulo
    Repositorio países
    
    Creando repositorio de países
    Obtener todos los países
    
    Método para obtener todos los países
    Obtener un país
    
    Método para obtener un país por su identificador
    Almacenar un país
    
    Método para almacenar un país
    Actualizar un país
    
    Método para actualizar un país
    Eliminar país
    
    Método para eliminar un país
    Estándar en nombre de métodos
    
    Cambiando el estándar para los nombres en los métodos del repositorio
    Prueba unitaria: Obtener todos los países
    
    Creación de set de prueba para obtener todos los países
    Prueba unitaria: Obtener un país por su identificador
    
    Creación de set de pruebas para obtener un país por su identificador
    Error solicitando token de acceso
    
    Error validando credenciales de acceso para generar token de acceso
    Sintaxis documentación
    
    Mejorando la sintaxis de la documentación en el módulo
    Archivos de configuraciones phpstorm
    
    Configuraciones para el editor phpstorm
    Pruebas unitarias: Almacenar país y validar campo nombre
    
    Método para almacenar un país y validar el nombre que se envía
    Pruebas unitaria: Actualizar país
    
    Creación de set de pruebas para actualizar un país
    Pruebas unitarias: Eliminar país
    
    Creación set de pruebas para eliminar un país
    
    * Revert "Merge remote-tracking branch 'origin/feature/module-country' into feature/module-country"
    
    This reverts commit 5fb693287a41c165f783b684d604f78437d4afa2.
    
    * Pruebas unitarias: Eliminar país
    
    Creación set de pruebas para eliminar un país

---
+++ Modules/Country/Database/Seeders/CountryDatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Country\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Modules\Country\Entities\Country;
+use Illuminate\Database\Eloquent\Model;
+
+class CountryDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        factory(Country::class, 50)->create();
+    }
+}
---
+++ Modules/Country/Tests/Unit/CreateItem.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Modules\Country\Tests\Unit;
+
+use Tests\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Modules\Country\Database\Seeders\CountryDatabaseSeeder;
+
+class CreateItem extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * Unit test: Insert country.
+     *
+     * @return void
+     */
+    public function testCountryCreateItem()
+    {
+        # Population table of countries
+        $this->seed(CountryDatabaseSeeder::class);
+
+        $response = $this->withHeaders([
+            "Accept" => "application/json"
+        ])
+            ->json("POST", "api/countries", [
+                 "name" => "Albania"
+            ]);
+        $response
+            ->assertCreated()
+            ->assertJsonStructure([
+                "data" => [
+                    "id", "name", "created", "updated"
+                ]
+            ]);
+    }
+
+    /**
+     * Unit test: Validate field name country.
+     *
+     * @return void
+     */
+    public function testCountryCreateItemValidateFieldName() {
+        $response = $this->withHeaders([
+            "Accept" => "application/json"
+        ])
+            ->json("POST", "api/countries");
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
